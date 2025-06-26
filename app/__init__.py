0ffa1d10ee6cfb3699650a40c3b05acb3718e413
ShowvenSuresh <showven1201@gmail.com>
2025-06-26 21:47:48 +0800

    music can play on page 1

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+import os 
+from flask import Flask, app, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def page1():
+    return render_template('page1.html')
+
+
+@app.route('/game')
+def page2():
+    return render_template('page2.html')
